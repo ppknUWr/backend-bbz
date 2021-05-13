6df352c77041b7e976dfbb20a095f828d1287dfe
xdexer <308167@uwr.edu.pl>
2021-05-14 01:20:46 +0200

    added dummy endpoints for testing purposes

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_message_api)
+    path('', views.welcome_message_api),
+    path('request_db', views.get_requested_db),
+    path('get_all_dbs', views.get_all_dbs)
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from django.apps import apps
 import datetime
 # Create your views here.
 
@@ -15,4 +16,45 @@ def welcome_message_api(request):
         "timestamp": int(datetime.datetime.now().timestamp())
     }
 
+    return Response(data)
+
+
+@api_view(["GET"])
+def get_requested_db(request):
+    selected_db = request.query_params.get('db')
+    db_exists = False
+    for model in apps.all_models['api']:
+        print(model)
+        if(model == selected_db):
+            db_exists = True
+
+    if db_exists:
+        data = {
+            "code": 1,
+            "message": "Your request: " + selected_db,
+            "timestamp": int(datetime.datetime.now().timestamp())
+        }
+    else:
+        data = {
+            "code": 2,
+            "message": "Error: DB does not exist !",
+            "timestamp": int(datetime.datetime.now().timestamp())
+        }
+    return Response(data)
+
+
+@api_view(["POST", "GET"])
+def get_all_dbs(request):
+    available_dbs = []
+    i = 0
+    for model in apps.all_models['api']:
+        available_dbs.append({i : model})
+        i += 1
+        print(model)
+
+    data = {
+        "code": 1,
+        "message": available_dbs,
+        "timestamp": int(datetime.datetime.now().timestamp())
+    }
     return Response(data)
