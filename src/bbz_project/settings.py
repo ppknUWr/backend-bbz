bd78e4197a180330575f186e2544ce62be161a39
xdexer <308167@uwr.edu.pl>
2021-05-14 17:44:29 +0200

    t

--- src/bbz_project/settings.py
+++ src/bbz_project/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/api/data'
+STATIC_URL = '/api/data/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
