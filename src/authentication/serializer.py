27010d9ce4902099935560cd6a734d1a1c3ea042
kacperma <310442@uwr.edu.pl>
2021-05-20 21:14:41 +0200

    added authentication and fixed database

---
+++ src/authentication/serializer.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password", "confirm_password", "date_joined")
+
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('confirm_password'):
+            raise serializers.ValidationError("Those passwords don't match.")
+        del attrs['confirm_password']
+        attrs['password'] = make_password(attrs['password'])
+        return attrs
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    default_error_messages = {
+        'inactive_account': 'User account is disabled.',
+        'invalid_credentials': 'Unable to login with provided credentials.'
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginSerializer, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self, attrs):
+        self.user = authenticate(username=attrs.get("username"), password=attrs.get('password'))
+        if self.user:
+            if not self.user.is_active:
+                raise serializers.ValidationError(self.error_messages['inactive_account'])
+            return attrs
+        else:
+            raise serializers.ValidationError(self.error_messages['invalid_credentials'])
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    auth_token = serializers.CharField(source='key')
+
+    class Meta:
+        model = Token
+        fields = ("auth_token", "created")
