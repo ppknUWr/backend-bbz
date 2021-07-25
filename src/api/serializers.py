6cd7f6ad7310c96a167093e1a25db9d7765bb906
xdexer <308167@uwr.edu.pl>
2021-07-25 12:18:51 +0200

    refactored the code and made comments

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -23,32 +23,30 @@ def serializer_prepare_model_names():
         data["result"]["code"] = 100 # CODE: Number of models returned from API is less/greater than dynamic models loaded into Django
     return data
 
+"""
+Function to serialize data from database model into JSON format
+@Param: db_id: int
+@Return: data: Dict -> Dictionary with all data from a model database
+"""
 def serializer_get_db_data(db_id):
-    #available_dbs = json_worker.get_models()['models'] #just debug
     selected_db = int(db_id) 
-    #print(available_dbs[selected_db], "\n")
-
     raw_db_data = models[selected_db]
 
-    #get all fields available in the model
-    print(raw_db_data._meta.get_fields(), "\n")
-
+    #get all fields available in the model in a form of list
     db_fields = [field.name for field in raw_db_data._meta.get_fields()]
-    print(db_fields)
-    #dumb as fuck
+    #check how many records are in the selected database
     querylength = len(list(raw_db_data.objects.values_list(db_fields[0], flat=True)))
-    #print(querylength)
+    #initialise JSON object to be retrieved
     db_data = {key : {} for key in range(0, querylength)}
-    #print(db_data)
 
+    #get all data from db into db_data
     for field in db_fields:
+        #get fields for selected param, e.g. all author fields
         fieldset = list(raw_db_data.objects.values_list(field, flat=True))
         index = 0
+        #put the field to corresponding dictionary
         for value in fieldset:
             db_data[index].update({field : value})
             index += 1
-        
-    #for x in range(querylength):
-    #    print(db_data[x], "\n")
-    
+
     return db_data
--- src/api/views.py
+++ src/api/views.py
@@ -28,32 +28,28 @@ def welcome_message_api(request):
 """
 get_requested_db
 Function to return data for requested DB from database
-@Param: selected_db_id: string 
+@Param: selected_db_id: int 
 @Return: JSON object with data of selected DB from django model
 """
 @api_view(["GET"])
 def get_requested_db(request):
     selected_db = request.query_params.get('db')
-    db_exists = False
-    available_dbs = serializers.serializer_get_db_data(selected_db)
-    # for model in apps.all_models['api']:
-    #     print(model)
-    #     if(model == selected_db):
-    #         db_exists = True
-
-    # if db_exists:
-    #     data = {
-    #         "code": 1,
-    #         "message": "Your request: " + selected_db,
-    #         "timestamp": int(datetime.datetime.now().timestamp())
-    #     }
-    # else:
-    #     data = {
-    #         "code": 2,
-    #         "message": "Error: DB does not exist !",
-    #         "timestamp": int(datetime.datetime.now().timestamp())
-    #     }
-    data = available_dbs
+    db_exists = True if int(selected_db) < len(apps.all_models['api']) else False
+    
+    if db_exists:
+        db_data = serializers.serializer_get_db_data(selected_db)
+        data = {
+            "code": 1,
+            "message": db_data,
+            "timestamp": int(datetime.datetime.now().timestamp())
+        }
+    else:
+        data = {
+            "code": 2,
+            "message": "Error: DB does not exist !",
+            "timestamp": int(datetime.datetime.now().timestamp())
+        }
+    
     return Response(data)
 
 """
@@ -71,10 +67,6 @@ def get_all_dbs(request):
         db = serializers.serializer_get_db_data(i)
         dbs_data.append({title : db})
         i += 1
-    # for model in apps.all_models['api']:
-    #     available_dbs.append({i : model})
-    #     i += 1
-    #     print(model)
 
     data = {
         "code": 1,
