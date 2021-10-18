a21df8a55644912141b9250a414360b3194ce0a1
HoopDead <polec0@wp.pl>
2021-10-18 20:54:47 +0200

    Refactored update_record with exceptions and db_id and record_id passed by JSON

--- src/api/views.py
+++ src/api/views.py
@@ -98,29 +98,55 @@ Function to update record in Django DB
 @Param: db_id: Str -> ID of DB to update
 @Param: record_id: Str -> ID of record to update
 """
-# TODO: Add validation if db_id and record_id is less (<=) than amount of dbs and record in it.
 @api_view(["PATCH"])
 def update_record(request):
-    # TODO: Change query_params to load json, not URL data.
     try:
-        db_id = int(request.query_params.get('db')) # TODO: Change query_params to db_id
-    except (ValueError, TypeError) as exception:
+        req = json.loads(request.body)
+    except json.JSONDecodeError:
+        return Response({
+            "code": 2,
+            "message": "Error, messed up JSON.",
+            "timestamp": str(int(datetime.datetime.now().timestamp()))
+        })
+    except Exception:
+        return Response({
+            "code": 2,
+            "message": "Error - unknow. Contact backend dev team.",
+            "timestamp": str(int(datetime.datetime.now().timestamp()))
+        })
+
+    # Acquire db_id from JSON given to endpoint.
+    try:
+        db_id = int(req['db_id'])
+    except (ValueError, TypeError, KeyError) as exception:
         return Response({
             "code": 2,
             "message": "Error, no database id provided",
             "timestamp": str(int(datetime.datetime.now().timestamp()))
         })
+    except Exception:
+        return Response({
+            "code": 2,
+            "message": "Error - unknow. Contact backend dev team.",
+            "timestamp": str(int(datetime.datetime.now().timestamp()))
+        })
 
+    # Acquire record_id from JSON given to endpoint.
     try:
-        record_id = int(request.query_params.get('record')) # TODO: Change query_params to record_id
-    except (ValueError, TypeError) as exception:
+        record_id = int(req['record_id'])
+    except (ValueError, TypeError, KeyError) as exception:
         return Response({
             "code": 2,
             "message": "Error, no record id provided",
             "timestamp": str(int(datetime.datetime.now().timestamp()))
         })
-
-    response = serializers.serializer_update_record(db_id, record_id, request.data) # db_id and record_id need to be casted into integers.
+    except Exception:
+        return Response({
+            "code": 2,
+            "message": "Error - unknow. Contact backend dev team.",
+            "timestamp": str(int(datetime.datetime.now().timestamp()))
+        })
+    response = serializers.serializer_update_record(db_id, record_id, req["data"])
     return Response(response)
 
 # TODO: Add description here. 
@@ -177,7 +203,7 @@ Function to remove revord from DB.
 def remove_record(request):
 
     try:
-      req = json.loads(request.body)
+      req = json.loads(request.body) # TODO: Fix indent here.
     except json.JSONDecodeError:
         return Response({
             "code": 2,
